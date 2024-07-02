52683b7a91e1c76494e26dec8af0c53810173053
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-07-03 02:45:16 +0300

    Host Directives

--- src/app/learning-resources/learning-resources.component.ts
+++ src/app/learning-resources/learning-resources.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SafeLinkDirecive } from '../safe-link.directive';
+import { LogDirective } from '../log.directive';
 
 @Component({
   selector: 'app-learning-resources',
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   standalone: true,
-  imports: [SafeLinkDirecive]
+  imports: [SafeLinkDirecive],
+  hostDirectives: [LogDirective]
 })
 export class LearningResourcesComponent { }
